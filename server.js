e468006b33c08b2cf5d27a63a6318bd272071dd0
pg8530391082 <pg8530391082@gmail.com>
2026-04-01 12:47:24 +0530

    Update server.js

--- server.js
+++ server.js
@@ -10,16 +10,17 @@ app.use(express.json())
 app.use(express.static(path.join(__dirname,"public")))
 
 let sensorData = {
-aqi:0,
-pm25:0,
-pm10:0,
-co:0,
-voc:0,
-so2:0,
-no2:0,
-nh3:0,
-temperature:0,
-humidity:0
+  aqi:0,
+  pm25:0,
+  pm10:0,
+  co:0,
+  voc:0,
+  so2:0,
+  no2:0,
+  nh3:0,
+  alcohol_aqi:0,
+  temperature:0,
+  humidity:0
 }
 
 app.post("/update",(req,res)=>{
